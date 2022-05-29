544cdcb34f08eb009b11f36c621c13a74a461b1c
EUNJEONGMUN <munej26@gmail.com>
2022-05-29 15:26:38 +0900

    [ADD] make order and discount domain and test

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        // 주문 생성 요청이 오면
+        Member member = memberRepository.findById(memberId); // 회원정보를 조회하고
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 할인정책에 회원을 넘김
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
