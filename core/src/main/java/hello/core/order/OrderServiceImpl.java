b929286585fc2d2f55587042bd3dacb6b5670e05
EUNJEONGMUN <munej26@gmail.com>
2022-05-31 16:10:02 +0900

    [ADD] Separation of roles and responsibilities

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,9 +9,23 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    /**
+     * 클라이언트 코드인 OrderServiceImpl은 DiscountPolicy의 인터페이스 뿐만 아니라 구체 클래스도 함께 의존한다.
+     * 그래서 구체 클래스를 변경할 때 클라이언트 코드도 함께 변경해야 한다.
+     * DIP 위반 -> 추상에만 의존하도록 변경(인터페이스에만 의존)
+     * DIP를 위반하지 않도록 인터페이스에만 의존하도록 의존 관계를 변경하면 된다.
+     */
+    private final DiscountPolicy discountPolicy; //
+    // OrderService 입장에서는 discountpolicy에 fixdiscountpolicy가 들어올지, ratediscountpolicy가 들어올지 전혀 알 수 없음
+    // 추상에만 의존하고 있음
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
