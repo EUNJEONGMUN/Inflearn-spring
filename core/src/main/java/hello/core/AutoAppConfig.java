bf3fdfb5e3198d211c5a2a7e8ed910ac30515bf0
EUNJEONGMUN <munej26@gmail.com>
2022-06-05 13:12:38 +0900

    [ADD] includeFilters, excludeFilters

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core", // 탐색할 패키지의 시작 위치 지정
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
 ) // @Component 가 붙은 클래스를 찾아 자동으로 빈으로 등록해줌
 public class AutoAppConfig {
