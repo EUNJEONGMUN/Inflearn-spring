24942545dac054683afc1c2b647b806ec4e8d200
EUNJEONGMUN <munej26@gmail.com>
2022-06-06 13:38:46 +0900

    [ADD] same name bean confliction test

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -10,4 +13,10 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)
 ) // @Component 가 붙은 클래스를 찾아 자동으로 빈으로 등록해줌
 public class AutoAppConfig {
+
+//    // 같은 이름충돌 테스트 용도
+//    @Bean(name="memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
