8f1db013f34d17714e0158cf82e12378b5c38f94
EUNJEONGMUN <munej26@gmail.com>
2022-06-07 13:05:32 +0900

    [ADD] use @PostConstruct, @PreDestroy annotation

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -24,6 +27,7 @@ public class NetworkClient {
         System.out.println("close = " + url);
     }
 
+    @PostConstruct
     public void init() {
         // afterPropertiesSet : 의존 관계 주입이 끝나면 호출하겠다는 뜻
         System.out.println("NetworkClient.init");
@@ -31,6 +35,7 @@ public class NetworkClient {
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close(){
         System.out.println("NetworkClient.close");
         disconnect();
